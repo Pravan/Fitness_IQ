efbd51bcdec062bb58407eb8eca04714099ec38b
Pravan <pravanjanb79@gmail.com>
2025-07-16 11:38:49 +0530

    SignIn screen

--- app/src/main/java/com/example/fit_iq/ui/Presentation/signin/SignInScreen.kt
+++ app/src/main/java/com/example/fit_iq/ui/Presentation/signin/SignInScreen.kt
@@ -1,3 +1,15 @@
 package com.example.fit_iq.ui.Presentation.signin
 
-import
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignInScreen(modifier: Modifier= Modifier){
+    Column{
+        Image(painter = , contentDescription = ")
+    }
+
+
+}
